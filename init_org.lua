b45378f603da5b79e5dad0ffa4d7bd5d695f8d1d
TheMaoci <themaoci@gmail.com>
2022-10-01 23:13:41 +0200

    duplicated original init just incase

---
+++ init_org.lua
@@ -0,0 +1,29 @@
+g_game.setLastSupportedVersion(1287)
+g_game.setCustomOs(6712)
+g_logger.setLogFile(g_resources.getWorkDir() .. g_app.getCompactName() .. ".log")
+g_logger.info(os.date("== application started at %b %d %Y %X"))
+g_logger.info(g_app.getName() .. ' ' .. g_app.getVersion() .. ' rev ' .. g_app.getBuildRevision() .. ' (' .. g_app.getBuildCommit() .. ') built on ' .. g_app.getBuildDate() .. ' for arch ' .. g_app.getBuildArch())
+
+if not g_resources.addSearchPath(g_resources.getWorkDir() .. "data", true) then
+  g_logger.fatal("Unable to add data directory to the search path.")
+end
+if not g_resources.addSearchPath(g_resources.getWorkDir() .. "modules", true) then
+  g_logger.fatal("Unable to add modules directory to the search path.")
+end
+g_resources.addSearchPath(g_resources.getWorkDir() .. "mods", true)
+g_resources.setupUserWriteDir(('%s/'):format(g_app.getCompactName()))
+g_resources.searchAndAddPackages('/', '.otpkg', true)
+g_configs.loadSettings("/config.otml")
+g_modules.discoverModules()
+-- libraries modules 0-99
+g_modules.autoLoadModules(99)
+g_modules.ensureModuleLoaded("corelib")
+g_modules.ensureModuleLoaded("gamelib")
+-- client modules 100-499
+g_modules.autoLoadModules(499)
+g_modules.ensureModuleLoaded("client")
+-- game modules 500-999
+g_modules.autoLoadModules(999)
+g_modules.ensureModuleLoaded("game_interface")
+-- mods 1000-9999
+g_modules.autoLoadModules(9999)

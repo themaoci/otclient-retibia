7211d010da69209c76828c89b3ac9205d086702c
TheMaoci <themaoci@gmail.com>
2022-10-01 23:13:46 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,16 +1,20 @@
 g_game.setLastSupportedVersion(1287)
---g_game.setCustomOs(9000)
+--g_game.setCustomOs(6712)
 g_logger.setLogFile(g_resources.getWorkDir() .. g_app.getCompactName() .. ".log")
-g_logger.info(os.date("== application started at %b %d %Y %X"))
+g_logger.info(os.date("== app started at %b %d %Y %X"))
 g_logger.info(g_app.getName() .. ' ' .. g_app.getVersion() .. ' rev ' .. g_app.getBuildRevision() .. ' (' .. g_app.getBuildCommit() .. ') built on ' .. g_app.getBuildDate() .. ' for arch ' .. g_app.getBuildArch())
 
-if not g_resources.addSearchPath(g_resources.getWorkDir() .. "data", true) then
-  g_logger.fatal("Unable to add data directory to the search path.")
+if not g_app.isPacked() then
+  if not g_resources.addSearchPath(g_resources.getWorkDir() .. "data", true) then
+    g_logger.fatal("Unable to add data directory to the search path.")
+  end
+  if not g_resources.addSearchPath(g_resources.getWorkDir() .. "modules", true) then
+    g_logger.fatal("Unable to add modules directory to the search path.")
+  end
+  g_resources.addSearchPath(g_resources.getWorkDir() .. "mods", true)
+else
+  g_resources.searchAndAddPackages("/", ".zip", true);
 end
-if not g_resources.addSearchPath(g_resources.getWorkDir() .. "modules", true) then
-  g_logger.fatal("Unable to add modules directory to the search path.")
-end
-g_resources.addSearchPath(g_resources.getWorkDir() .. "mods", true)
 g_resources.setupUserWriteDir(('%s/'):format(g_app.getCompactName()))
 g_resources.searchAndAddPackages('/', '.otpkg', true)
 g_configs.loadSettings("/config.otml")
